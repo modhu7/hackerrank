fce3cf11019c804943e18e1a0d09685ebe132884
SSGupta <modhu7@gmail.com>
2016-07-26 22:38:26 +0600

    Add day 2
    Signed-off-by: SSGupta <modhu7@gmail.com>

--- hackerrank-java/src/main/java/info/ssgupta/hackerrank/java/tutorial/Day2Operators.java
+++ hackerrank-java/src/main/java/info/ssgupta/hackerrank/java/tutorial/Day2Operators.java
@@ -5,10 +5,29 @@
  */
 package info.ssgupta.hackerrank.java.tutorial;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ssgupta
  */
 public class Day2Operators {
-  
+
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    double mealCost = scan.nextDouble(); // original meal price
+    int tipPercent = scan.nextInt(); // tip percentage
+    int taxPercent = scan.nextInt(); // tax percentage
+    scan.close();
+
+    // Write your calculation code here.
+    double doubleTipPercent = (double) tipPercent;
+    double doubleTaxPercent = (double) taxPercent;
+    // cast the result of the rounding operation to an int and save it as totalCost 
+    int totalCost = (int) Math.round(mealCost*(doubleTipPercent/100) + mealCost*(doubleTaxPercent/100) + mealCost);
+    System.out.println("The total cost of meal is " + totalCost + " dollars.s");
+
+    // Print your result
+  }
+
 }
