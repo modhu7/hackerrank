be569b84dff2289179ade946b5c82182d72e4e0c
SSGupta <modhu7@gmail.com>
2016-07-26 23:59:38 +0600

    algorithm
    Signed-off-by: SSGupta <modhu7@gmail.com>

---
+++ hackerrank-java/src/main/java/info/ssgupta/hackerrank/java/algorithm/AVeryBigSum.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.ssgupta.hackerrank.java.algorithm;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ssgupta
+ */
+public class AVeryBigSum {
+
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+    int arr[] = new int[n];
+    long sum = 0;
+    for (int arr_i = 0; arr_i < n; arr_i++) {
+      arr[arr_i] = in.nextInt();
+      sum = sum + (long) arr[arr_i];
+    }
+    System.out.println(sum);
+  }
+}
