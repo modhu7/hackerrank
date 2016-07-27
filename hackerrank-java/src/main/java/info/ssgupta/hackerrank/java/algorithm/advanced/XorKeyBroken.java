ad97bd2f9030d7c96daff61e0358128b94e13dfa
Subrata Sen Gupta <subrata@escenic.com>
2016-07-27 17:10:24 +0600

    xorkey
    Signed-off-by: Subrata Sen Gupta <subrata@escenic.com>

---
+++ hackerrank-java/src/main/java/info/ssgupta/hackerrank/java/algorithm/advanced/XorKeyBroken.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.ssgupta.hackerrank.java.algorithm.advanced;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ssgupta
+ */
+public class XorKeyBroken {
+
+  public static void main(String args[]) {
+    Scanner in = new Scanner(System.in);
+    int T = in.nextInt();
+    for(int i=0;i<T;i++){
+    }
+  }
+}
